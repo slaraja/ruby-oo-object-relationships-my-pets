a1d5f35d98185d4ab1a806035f73cef948624dd8
Stephanie La Raja <stephanielaraja@gmail.com>
2021-02-08 22:08:41 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Cat
-  # code goes here
-end
+attr_reader :name 
+attr_accessor :owner, :mood
+
+@@all=[]
+  def initialize(name, owner)
+    @owner=owner
+    @name=name
+    @mood= "nervous"
+    @@all<<self
+    @owner.cats<<self
+  end 
+  
+  def self.all
+    @@all
+  end
+ end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
 class Owner
-  # code goes here
+
+  attr_accessor :cats, :dogs
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name=name
+    @species="human"
+    @@all<<self
+    @cats=[]
+    @dogs=[]
+  end
+
+  def species
+    @species
+  end 
+
+  def say_species
+     "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end 
+ 
+  def buy_cat(name)
+    Cat.new(name,self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name,self)
+  end 
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood="happy"}
+  end 
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood="happy"}
+  end
+
+  def sell_pets
+    self.dogs.each do|dog| 
+      dog.mood="nervous"
+      dog.owner=nil
+      dogs.pop
+    end
+
+    self.cats.each do |cat|
+      cat.mood="nervous"
+      cat.owner=nil
+      cats.pop
+    end 
+
+  end
+
+  def list_pets
+   "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end
+  
 end
